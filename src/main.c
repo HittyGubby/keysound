e3ab99f4e0e762314f3f15371f02ac3abfd9b433
Shyam-Jagath <shyamjagath99@gmail.com>
2026-05-06 01:03:08 +0530

    add logic for creating symlink for the first time

--- src/main.c
+++ src/main.c
@@ -4,6 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+
+#define CONFIG_PATH ".config/keysound"
+#define CURRENT_LINK "current"
 
 volatile sig_atomic_t keep_running = 1;
 
@@ -13,9 +21,79 @@ void handle_signal(int sig){
     }
 }
 
+int dir_exists(const char *dir){
+
+    struct stat st;
+    if(stat(dir,&st)!= 0){
+        return 0;
+    }
+    if(S_ISDIR(st.st_mode)){
+        return 1;
+    }
+    return 0;
+}
+
 void on_key_pressed(int keycode){
     KeyGroup group = get_group(keycode);
     audio_play_group(group);
+}
+
+void link_to_any_random_soundpack(const char *config_path , const char *link_path){
+    DIR *dir = opendir(config_path);
+    if(!dir){
+        return;
+    }
+
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL){
+
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0 || strcmp(entry->d_name, CURRENT_LINK) == 0){
+            continue;
+        }
+
+        char full_path[PATH_MAX];
+        snprintf(full_path, sizeof(full_path), "%s/%s", config_path, entry->d_name);
+        
+        if (dir_exists(full_path)){
+            printf("no 'default' folder found, falling back to pack '%s'\n", entry->d_name);
+            
+            if (symlink(full_path, link_path) != 0){
+                perror("error creating fallback symlink");
+            }
+            closedir(dir);
+            return; 
+        }
+    }
+    closedir(dir);
+
+    printf("wtf?, the keysound directory is totally empty, please add soundpacks to %s\n", config_path);
+}
+
+void ensure_default_pack(){
+    const char *home = getenv("HOME");
+    if (!home) return;
+
+    char base_path[PATH_MAX];
+    char link_path[PATH_MAX];
+    char default_pack_path[PATH_MAX];
+
+    snprintf(base_path, sizeof(base_path), "%s/%s", home, CONFIG_PATH);
+    snprintf(link_path, sizeof(link_path), "%s/%s", base_path, CURRENT_LINK);
+    snprintf(default_pack_path, sizeof(default_pack_path), "%s/default", base_path);
+
+    if (access(link_path, F_OK) == 0){
+        return; 
+    }
+
+    if (dir_exists(default_pack_path)){
+        printf("First run detected: Linking 'current' to the 'default' soundpack.\n");
+        
+        if (symlink(default_pack_path, link_path) != 0) {
+            perror("Error: Failed to create default symlink");
+        }
+    } else {
+        link_to_any_random_soundpack(base_path,link_path);
+    }
 }
 
 int main(int argc, char *argv[]){
@@ -28,6 +106,7 @@ int main(int argc, char *argv[]){
     sigaction(SIGTERM, &sa, NULL);
 
     printf("Starting keysound daemon...\n");
+    ensure_default_pack();
 
     audio_init();
 
